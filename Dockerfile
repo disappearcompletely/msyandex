3e7261e9e4dd7189db892e6d207c0bf7a962d834
disappearcompletely <d.komarov2019@gmail.com>
2024-02-02 22:10:50 +0300

    db

--- Dockerfile
+++ Dockerfile
@@ -4,4 +4,5 @@ COPY requirements.txt requirements.txt
 RUN pip install --no-cache-dir -r requirements.txt
 COPY . .
 ENV FLASK_APP=app.py
-CMD ["flask", "run", "--host=0.0.0.0", "--port=${PORT:-5000}"]
+CMD flask run --host=0.0.0.0 --port=${PORT:-5000}
+
--- app.py
+++ app.py
@@ -7,7 +7,8 @@ import os
 
 app = Flask(__name__)
 metrics = PrometheusMetrics(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///C:/Users/Dmitriy/Desktop/msyandex/data/food_ordering.db'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///C:/Users/Dmitriy/Desktop/msyandex/data/food_ordering.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///food_ordering.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
