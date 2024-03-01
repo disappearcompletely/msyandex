858d36d2387b96a16e93ce98a4bb5041c2016c08
disappearcompletely <d.komarov2019@gmail.com>
2024-03-01 03:25:19 +0300

    fix

--- app.py
+++ app.py
@@ -3,16 +3,20 @@ from flask import request, jsonify, render_template
 from models import db, MenuItem, Order, Reservation
 from datetime import datetime
 from prometheus_flask_exporter import PrometheusMetrics
-import os
 
 app = Flask(__name__)
 metrics = PrometheusMetrics(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'postgresql://dmitriy:321123@localhost/club')
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///C:/Users/Dmitriy/Desktop/prac6/data/food_ordering.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///food_ordering.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
 
 is_db_initialized = False
+
+@app.route('/')
+def home():
+    return render_template('index.html')
 
 @app.before_request
 def create_tables():
@@ -21,26 +25,22 @@ def create_tables():
         db.create_all()
         is_db_initialized = True
 
-@app.route('/')
-def home():
-    return render_template('index.html')
-
 @app.route('/menu', methods=['POST'])
 def add_menu_item():
-        data = request.json
-        name = data.get('name')
-        price = data.get('price')
-        if not name or price is None:
-            return jsonify({"error": "Имя или цена пропущены"}), 400
-        new_item = MenuItem(name=name, price=price)
-        db.session.add(new_item)
-        db.session.commit()
-        return jsonify({"message": "Позиция была создана", "id": new_item.id}), 201
+    data = request.json
+    name = data.get('name')
+    price = data.get('price')
+    if not name or price is None:
+        return jsonify({"error": "Missing name or price"}), 400
+    new_item = MenuItem(name=name, price=price)
+    db.session.add(new_item)
+    db.session.commit()
+    return jsonify({"message": "Позиция была создана", "id": new_item.id}), 201
 
 @app.route('/menu', methods=['GET'])
 def get_menu():
-        menu_items = MenuItem.query.all()
-        return jsonify(menu=[{'id': item.id, 'name': item.name, 'price': item.price} for item in menu_items])
+    menu_items = MenuItem.query.all()
+    return jsonify(menu=[{'id': item.id, 'name': item.name, 'price': item.price} for item in menu_items])
 
 @app.route('/order', methods=['POST'])
 def make_order():
@@ -66,10 +66,10 @@ def make_order():
 
 @app.route('/orders', methods=['GET'])
 def get_orders():
-        orders = Order.query.all()
-        return jsonify(orders=[
-            {'id': order.id, 'items': order.items, 'total_price': order.total_price, 'order_time': order.order_time} for
-            order in orders])
+    orders = Order.query.all()
+    return jsonify(orders=[
+        {'id': order.id, 'items': order.items, 'total_price': order.total_price, 'order_time': order.order_time} for
+        order in orders])
 
 @app.route('/reserve', methods=['POST'])
 def reserve_seat():
@@ -85,16 +85,16 @@ def reserve_seat():
 
 @app.route('/reservations', methods=['GET'])
 def get_reservations():
-        reservations = Reservation.query.all()
-        reservations_list = [
-            {
-                'id': reservation.id,
-                'user_name': reservation.user_name,
-                'seat_number': reservation.seat_number,
-                'time': reservation.time.strftime('%Y-%m-%d %H:%M:%S')
-            } for reservation in reservations
-        ]
-        return jsonify(reservations=reservations_list)
+    reservations = Reservation.query.all()
+    reservations_list = [
+        {
+            'id': reservation.id,
+            'user_name': reservation.user_name,
+            'seat_number': reservation.seat_number,
+            'time': reservation.time.strftime('%Y-%m-%d %H:%M:%S')
+        } for reservation in reservations
+    ]
+    return jsonify(reservations=reservations_list)
 
 @app.route('/check_availability', methods=['POST'])
 def check_availability():
@@ -117,5 +117,4 @@ def cancel_reservation(reservation_id):
 
 
 if __name__ == '__main__':
-    port = int(os.getenv('PORT', '5002'))
-    app.run(debug=True, port=port)
+    app.run(debug=True, port=5002)
