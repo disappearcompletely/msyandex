7b6672010685807da7448c518fea819bfae50c06
disappearcompletely <d.komarov2019@gmail.com>
2024-03-01 03:01:36 +0300

    fix

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from flask import request, jsonify, render_template
 from models import db, MenuItem, Order, Reservation
 from datetime import datetime
 from prometheus_flask_exporter import PrometheusMetrics
-from keycloak import KeycloakOpenID
 import os
 
 app = Flask(__name__)
@@ -15,32 +14,6 @@ db.init_app(app)
 
 is_db_initialized = False
 
-keycloakid = KeycloakOpenID(server_url='http://keycloak:8080/', client_id='mscc', realm_name='cc', client_secret_key='sjftvpMkf1t5IEl0bw0IXwjjIE0HXkOq')
-
-token = ''
-
-def token_get(password, user_name):
-    global token
-    token = keycloakid.token(grant_type=['password'], username = user_name, password = password)
-
-@app.route('/login')
-def login():
-    login = request.args.get('login')
-    password = request.args.get('password')
-    token_get(password, login)
-    return '''<h1>Logged in</h1>'''.format(login)
-
-def check_user_roles():
-    global token
-    try:
-        userinfo = keycloakid.userinfo(token["access_token"])
-        token_info = keycloakid.introspect(token["access_token"])
-        if "moder" not in token_info["realm_access"]["roles"]:
-            print('no role')
-        return token_info
-    except Exception as e:
-        return ('error')
-
 @app.before_request
 def create_tables():
     global is_db_initialized
@@ -50,14 +23,10 @@ def create_tables():
 
 @app.route('/')
 def home():
-    if check_user_roles() != 'error':
-        return render_template('index.html')
-    else:
-        return('please,login')
+    return render_template('index.html')
 
 @app.route('/menu', methods=['POST'])
 def add_menu_item():
-    if check_user_roles():
         data = request.json
         name = data.get('name')
         price = data.get('price')
@@ -67,16 +36,11 @@ def add_menu_item():
         db.session.add(new_item)
         db.session.commit()
         return jsonify({"message": "Позиция была создана", "id": new_item.id}), 201
-    else:
-        return('please,login')
 
 @app.route('/menu', methods=['GET'])
 def get_menu():
-    if check_user_roles():
         menu_items = MenuItem.query.all()
         return jsonify(menu=[{'id': item.id, 'name': item.name, 'price': item.price} for item in menu_items])
-    else:
-        return ('please, login')
 
 @app.route('/order', methods=['POST'])
 def make_order():
@@ -102,13 +66,10 @@ def make_order():
 
 @app.route('/orders', methods=['GET'])
 def get_orders():
-    if check_user_roles():
         orders = Order.query.all()
         return jsonify(orders=[
             {'id': order.id, 'items': order.items, 'total_price': order.total_price, 'order_time': order.order_time} for
             order in orders])
-    else:
-        return('please, login')
 
 @app.route('/reserve', methods=['POST'])
 def reserve_seat():
@@ -124,7 +85,6 @@ def reserve_seat():
 
 @app.route('/reservations', methods=['GET'])
 def get_reservations():
-    if check_user_roles():
         reservations = Reservation.query.all()
         reservations_list = [
             {
@@ -135,8 +95,6 @@ def get_reservations():
             } for reservation in reservations
         ]
         return jsonify(reservations=reservations_list)
-    else:
-        return('please, login')
 
 @app.route('/check_availability', methods=['POST'])
 def check_availability():
